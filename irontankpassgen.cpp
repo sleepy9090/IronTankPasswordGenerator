9956e6f21dd5d5d56ed867c85f7a9397dd4702f5
Shawn M. Crawford <sleepy3d@gmail.com>
2019-03-22 11:34:41 -0400

    Add files via upload

--- irontankpassgen.cpp
+++ irontankpassgen.cpp
@@ -36,8 +36,8 @@ int main (int argc, char *argv[])
 {
     int level = 0;
     int tempArray[7];
-    int r = 0;
-    int i = 0;
+    int x = 0;
+    int y = 0;
     string input = "";
 
     cout << " _______                     _______               __    " << endl;
@@ -66,28 +66,28 @@ int main (int argc, char *argv[])
         cout << "Invalid number, please try again." << endl;
     }
 
-    r = (level & 256) >> 8 | (level & 255) << 8;
-    r |= ((r & 255 ^ level & 255) << 1 | r & 255) & 255;
-    level = (r & 255) + (r >> 8 & 255) + 171;
+    x = (level & 256) >> 8 | (level & 255) << 8;
+    x |= ((x & 255 ^ level & 255) << 1 | x & 255) & 255;
+    level = (x & 255) + (x >> 8 & 255) + 171;
     tempArray[0] = (level & 112) >> 4;
     tempArray[6] = level & 7;
     level = 10000;
     for(int k = 0; k < 5; k++)
     {
-        i = 0;
-        while(r - level >= 0)
+        y = 0;
+        while(x - level >= 0)
         {
-            r -= level;
-            i++;
+            x -= level;
+            y++;
         }
         level = level / 10;
-        tempArray[k+1] = i;
+        tempArray[k+1] = y;
     }
 
     string password;
-	for(int i = 0; i < 7; i++)
+	for(int y = 0; y < 7; y++)
     {
-        password += "0123456789"[tempArray[i]];
+        password += "0123456789"[tempArray[y]];
     }
 
     cout << endl;
